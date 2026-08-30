4bc506f6ffeec08de19b34d7c3c08ae448910feb
Ligiwa Gabriel Badi <ligiwagabriel@gmail.com>
2026-08-30 11:48:02 +0300

    determine grades

---
+++ exam.py
@@ -0,0 +1,14 @@
+## DETERMINE STUDENTS GRADES
+Age=int(input("Enter your age "))
+Marks=float(input("Enter your Marks"))
+
+if Marks>=80:
+    print("A")
+elif Marks>=70:
+    print("B")
+elif Marks>=60:
+    print("C")
+elif Marks>=50:
+    print("D")
+else:
+    print("You have failed")
