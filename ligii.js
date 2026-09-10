3fe7ad63ab31a572464c112d62036e552c275393
Ligiwa Gabriel Badi <ligiwagabriel@gmail.com>
2026-09-10 09:46:58 +0300

    more functionality

--- ligii.js
+++ ligii.js
@@ -6,6 +6,15 @@ let isRegistered = false;
 let total = a+b;
 console.log(total);
 
+let courseWorkMarks = 30;
+let examMarks = 30;
+let totalMarks = courseWorkMarks + examMarks;
+if(totalMarks>50){
+    console.log("You have passed")
+}else{
+    console.log("You have a retake")
+}
+
 
 
 
